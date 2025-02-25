01ba74b808473d2694a9bcb8768a8b46b79305a8
Roo-CS <195388339+Roo-CS@users.noreply.github.com>
2025-02-24 20:24:51 -0500

    Completed assignment #2 - Add and test: functionality for the repeatString method

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(text, num) {
+  
+  if(num >= 0){
+    let repeatedString = "";
+    
+    for(let i = 0; i < num; i++){
+      repeatedString += text;
+    }
 
+    return repeatedString;
+  }
+
+  return "ERROR";
 };
 
 // Do not edit below this line
