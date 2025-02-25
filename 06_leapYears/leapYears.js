9d44d86518b1a046de3c87195f26c2ba5e9790b5
Roo-CS <195388339+Roo-CS@users.noreply.github.com>
2025-02-24 22:37:49 -0500

    Completed exercise #6 - Add and test: functionality added for leapYears function to calculate if a year is a leap year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,18 @@
-const leapYears = function() {
+/*
+  Given: a year (int)
 
+  Leap years:
+    - Divisible by 4
+    - Divisible by 100, ARE NOT unless divisible by 400, ARE
+*/
+const leapYears = function(year) {
+  if(year % 4 == 0){
+    if(year % 100 == 0 && year % 400 != 0){
+      return false;
+    }
+    return true;
+  }
+  return false;
 };
 
 // Do not edit below this line
