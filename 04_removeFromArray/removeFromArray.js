04011740d0bbc67295855e4fa9e41365ea97ce92
Roo-CS <195388339+Roo-CS@users.noreply.github.com>
2025-02-24 21:08:21 -0500

    Completed exercise #4 - Add and test: functionality for removeFromArray method

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...toDelete) {
+
+  let deletedNums = new Set(toDelete);
+  let cleanedArray = [];
+  for(const num of inputArray){
+    if(!deletedNums.has(num)){
+      cleanedArray.push(num);
+    }
+  }
+
+  return cleanedArray;
+
 };
 
 // Do not edit below this line
