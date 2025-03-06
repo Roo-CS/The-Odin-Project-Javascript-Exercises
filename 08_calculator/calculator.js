dd23ff64163e1520f2f754779f25ba9089b4243f
Roo-CS <195388339+Roo-CS@users.noreply.github.com>
2025-03-06 18:56:42 -0500

    Completed assignment #8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,20 @@
-const add = function() {
-	
-};
+const add = (a, b) => a + b;
 
-const subtract = function() {
-	
-};
+const subtract = (a, b) => a - b;
 
-const sum = function() {
-	
-};
+const sum = (arr) => arr.reduce(add, 0);
 
-const multiply = function() {
+const multiply = (arr) => arr.reduce((a, b) => a * b);
 
-};
+const power = (a, b) => a ** b;
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = (num) => {
+  if(num === 0) return 1;
+  let total = 1;
+  for(let i = 1; i <= num; i++){
+    total = i * total;
+  }
+  return total;
 };
 
 // Do not edit below this line
