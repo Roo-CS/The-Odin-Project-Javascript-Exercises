7560c4715b94409db9bf517604ea9dda4e44ca91
Roo-CS <195388339+Roo-CS@users.noreply.github.com>
2025-03-07 15:27:22 -0500

    Completed assignment #10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = (num) =>{
+ 
+  if(num === 0 || +num === 0) return 0;
+  num = +num;
 
+  let fibArray = [1, 1];
+  let i = 0;  
+
+  if(num > 0){
+    while(fibArray.length < num){
+      fibArray.push(fibArray[i] + fibArray[i+1]);
+      i++;
+    }
+
+    return fibArray[fibArray.length-1];
+  }
+
+  return "OOPS";
 };
 
 // Do not edit below this line
