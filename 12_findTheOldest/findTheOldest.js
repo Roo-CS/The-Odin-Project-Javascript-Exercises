e754065949674d89d9afb99a0c1ec2c1112369d5
Roo-CS <195388339+Roo-CS@users.noreply.github.com>
2025-03-07 17:06:41 -0500

    Completed assignment #12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = (people) => {
+  let currOldAge = Number.MIN_VALUE;
+  let oldestPerson;
 
-};
+  for(const person of people){
+    if(!person.yearOfDeath){
+      death = new Date().getFullYear();
+      person.yearOfDeath = death;
+    }
+    person.age = person["yearOfDeath"] - person["yearOfBirth"];
+
+    if(person.age > currOldAge){
+      currOldAge = person.age;
+      oldestPerson = person;
+    }
+  }
+  return oldestPerson;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
